6aef71f8521fdc5cb9aa35486cd9ef74fc0df234
LuanScudeler <luanscudeler@gmail.com>
2016-08-27 18:03:53 -0300

    + Added notification for messages received from different contacts from the current one

--- app/src/main/java/br/chatup/tcc/activity/ChatActivity.java
+++ app/src/main/java/br/chatup/tcc/activity/ChatActivity.java
@@ -1,15 +1,21 @@
 package br.chatup.tcc.activity;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -82,10 +88,46 @@ public class ChatActivity extends AppCompatActivity {
                 displayMessage(message);
             else {
                 Log.d(TAG, "[BroadcastReceiver] Raising notification");
-                //TODO: Raise notification to start chat with a different contact from the current one
+                raiseNotification();
             }
         }
     };
+
+    //TODO: Fix bugs with listener when accessing the notification and pass the required values for the activity (intent)
+    private void raiseNotification() {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+            .setSmallIcon(R.drawable.notification_icon_mdpi)
+            .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.notification_icon_xhdpi))
+            .setTicker("New message!")
+            .setContentTitle("Message from [contact]")
+            .setContentText("[Message content]");
+
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, ChatActivity.class);
+        resultIntent.putExtra("contactJID", contactJID);
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(ChatActivity.class);
+
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        // Get Android notification service
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Configure
+        Notification n = mBuilder.build();
+        n.vibrate = new long[]{150, 300, 150, 600};
+
+        // First parameter refers to notification id, notification can be modified later
+        mNotificationManager.notify(R.drawable.notification_icon_mdpi, n);
+    }
 
     @Override
     protected void onStart() {
